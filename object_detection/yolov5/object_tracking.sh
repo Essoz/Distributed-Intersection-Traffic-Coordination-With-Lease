0084d16c9f114e71622eb051b5839073d6339c52
Simon Hu <xinwen.19@intl.zju.edu.cn>
2023-05-08 04:53:32 -0400

    sudo -E

--- object_detection/yolov5/object_tracking.sh
+++ object_detection/yolov5/object_tracking.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo python3 object_tracking.py --weights ./yolov5n.pt --imgsz 800 1024 --conf-thres 0.25 --classes 0 2 3 5 7 9 11 56
+sudo -E python3 object_tracking.py --weights ./yolov5n.pt --imgsz 800 1024 --conf-thres 0.25 --classes 0 2 3 5 7 9 11 56
